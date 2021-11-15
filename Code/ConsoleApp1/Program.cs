73754a891e6a64f347d661f92e9d6125bb3c4506
Venugopal  K (C) <vgk@tritechsamvit.com>
2021-11-15 18:13:00 +0530

    Threading Updated

--- Code/ConsoleApp1/Program.cs
+++ Code/ConsoleApp1/Program.cs
@@ -6,58 +6,77 @@ namespace ConsoleApp1
     
     public class DBWriter
     {
+        object _syncObjForWriters = new object();
+        object _syncObjForLog = new object();
         //public void Insert(DBWriter this){}
         public void Insert() {
             Console.WriteLine("Insert Started...");
-            Monitor.Enter(this);
-            for (int i = 0; i < 5; i++)
+            try
             {
-                Console.WriteLine($"Inserting Data...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
-                System.Threading.Thread.Sleep(1000);
+                Monitor.Enter(_syncObjForWriters);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Inserting Data...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                    System.Threading.Thread.Sleep(1000);
+                    if (i == 2) { return; }
+                }
+                
             }
-            Monitor.Exit(this);
+            finally
+            {
+                Monitor.Exit(_syncObjForWriters);
+            }
             Console.WriteLine("Insert End...");
         }
     
         public void Update() {
             //public void Update(DBWriter this){}
             Console.WriteLine("Update Started...");
-            Monitor.Enter(this);
-            for (int i = 0; i < 5; i++)
+            //Monitor.Enter(_syncObjForWriters);
+            lock (_syncObjForWriters)
             {
-                Console.WriteLine($"Updating Data...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
-                System.Threading.Thread.Sleep(1000);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Updating Data...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                    System.Threading.Thread.Sleep(1000);
+                }
             }
-            Monitor.Exit(this);
+            //Monitor.Exit(_syncObjForWriters);
             Console.WriteLine("Update End...");
 
         }
         //public void Delete(DBWriter this){}
         public void Delete() {
             Console.WriteLine("Delete Started...");
-            Monitor.Enter(this);
+            Monitor.Enter(_syncObjForWriters);
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"Deleting Data...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
                 System.Threading.Thread.Sleep(1000);
             }
-            Monitor.Exit(this);
+            Monitor.Exit(_syncObjForWriters);
             Console.WriteLine("Delete End...");
         }
         public void Log()
         {
-            
+            Monitor.Enter(_syncObjForLog);
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine($"Logging  Operation...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
                 System.Threading.Thread.Sleep(1000);
             }
-            
+            Monitor.Exit(_syncObjForLog);
+
         }
 
     }
     class Program
     {
+        static ManualResetEvent _waitHanlde = new ManualResetEvent(false);
+        static AutoResetEvent _waitHanldeBgTask1 = new AutoResetEvent(false);
+        static AutoResetEvent _waitHanldeBgTask2 = new AutoResetEvent(false);
+        static AutoResetEvent _waitHanldeBgTask3 = new AutoResetEvent(false);
+
         static void Main_old(string[] args)
         {
             Console.WriteLine($"Main method -Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
@@ -77,7 +96,7 @@ namespace ConsoleApp1
 
         }
 
-        static void Main()
+        static void Main_new_old()
         {
             DBWriter _dbWriterRef = new DBWriter();
             new System.Threading.Thread(new ThreadStart(_dbWriterRef.Insert)).Start() ;
@@ -105,6 +124,75 @@ namespace ConsoleApp1
             }
         }
 
+        static void Main()
+        {
+            //WaitCallback _bgTaskAddress = new WaitCallback(BgTask);
+            //ThreadPool.QueueUserWorkItem(_bgTaskAddress);
+            ThreadPool.QueueUserWorkItem(BgTaskOne);
+            ThreadPool.QueueUserWorkItem(BgTaskTwo);
+            ThreadPool.QueueUserWorkItem(BgTaskThree);
+            Thread.Sleep(3000);
+            _waitHanlde.Set();
+            WaitHandle.WaitAny(new WaitHandle[] { _waitHanldeBgTask1,_waitHanldeBgTask2,_waitHanldeBgTask3 });
+            Console.WriteLine("Main Completed");
+        }
+        static void BgTask(object args)
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"BgTask...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+
+        static void BgTaskOne(object args)
+        {
+            Console.WriteLine($"BgTask1...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId} Awaiting For Signal from Main Thread");
+            _waitHanlde.WaitOne();
+            
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"BgTask1...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                System.Threading.Thread.Sleep(1000);
+                //5 iteration
+                if (i == 5)
+                {
+                    _waitHanldeBgTask1.Set();
+
+                }
+            }
+        }
+        static void BgTaskTwo(object args)
+        {
+          
+            Console.WriteLine($"BgTask2...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId} Awaiting For Signal from Main Thread");
+            _waitHanlde.WaitOne();
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"BgTask2...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                System.Threading.Thread.Sleep(1000);
+                //10
+                if (i == 10) { _waitHanldeBgTask2.Set(); }
+            }
+        }
+        static void BgTaskThree(object args)
+        {
+            
+            Console.WriteLine($"BgTask3...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId} Awaiting For Signal from Main Thread");
+            _waitHanlde.WaitOne();
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"BgTask3...-Thread Id {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                System.Threading.Thread.Sleep(1000);
+                //12
+                if (i == 12)
+                {
+                    _waitHanldeBgTask3.Set();
+
+                }
+            }
+        }
 
     }
 }
