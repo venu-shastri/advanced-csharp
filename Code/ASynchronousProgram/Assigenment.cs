e90a498a6babd713449e59a0b0e1509e13aeda99
Venugopal  K (C) <vgk@tritechsamvit.com>
2021-11-16 18:06:41 +0530

    TPL Updated

--- Code/ASynchronousProgram/Assigenment.cs
+++ Code/ASynchronousProgram/Assigenment.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace ASynchronousProgram
 {
-    class Assigenment
+    public class Assigenment
     {
+
+
+        static void SignDocumnetUsingDS(string documentContent)
+        {
+           string santizedDocContent= SantizeDocumentContent(documentContent);
+           string digest= HashSantizedDocumentContent(santizedDocContent);
+           string signedDocumentContent = EncryptDigestUsingPrivateKey(digest);
+            Console.WriteLine(signedDocumentContent);
+        }
+
+        static void SignDocumnetUsingDSAsync(string documentContent)
+        {
+
+
+        }
+       static string SantizeDocumentContent(string documentContent)
+        {
+            Task.Delay(3000).Wait();
+            return documentContent + " Santized ";
+        }
+        static string HashSantizedDocumentContent(string santizedDocumentContent)
+        {
+            Task.Delay(3000).Wait();
+            return santizedDocumentContent + " Hashed ";
+        }
+        static string EncryptDigestUsingPrivateKey(string digest)
+        {
+            Task.Delay(3000).Wait();
+            return digest  + " Encrypted ";
+        }
     }
 }
--- Code/ASynchronousProgram/BreakFast.cs
+++ Code/ASynchronousProgram/BreakFast.cs
@@ -11,6 +11,7 @@ namespace ASynchronousProgram
         {
             static void Main(string[] args)
             {
+          
             //PrepareBreakFastSynchronously();
             // PrepareBreakFastAsynchronously();
             //PrepareBreakfastAsyncUsingStateMachine();
